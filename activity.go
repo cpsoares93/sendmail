02c7428896b838541c14ca8fd031955f53c8fc13
carolina.soares <carolina.soares@litthub.com>
2020-05-04 17:42:47 +0100

    add date

--- activity.go
+++ activity.go
@@ -44,7 +44,7 @@ func (a *sendmail) Eval(ctx activity.Context) (done bool, err error) {
 	speciality := ctx.GetInput("speciality").(*string)
 	patient := ctx.GetInput("patient").(*string)
 	practitioner := ctx.GetInput("practitioner").(*string)
-	//date := ctx.GetInput("date").(*)
+	date := ctx.GetInput("date").(*string)
 	clinic := "?"
 
 	clientAppointment:= *appointment
@@ -52,7 +52,7 @@ func (a *sendmail) Eval(ctx activity.Context) (done bool, err error) {
 	clientPatient := *patient
 	clientPractitioner := *practitioner
 	ercpnt := *vercpnt;
-	//clientDate := *date
+	clientDate := *date
 
 	auth := smtp.PlainAuth("", sender, apppass, server)
 	templateData := struct {
@@ -68,7 +68,7 @@ func (a *sendmail) Eval(ctx activity.Context) (done bool, err error) {
 		Appointment:  clientAppointment,
 		Speciality: clientsSpeciality,
 		Practitioner: clientPractitioner,
-		Date: "?",
+		Date: clientDate,
 		Hour: "?",
 		Local: "?",
 	}

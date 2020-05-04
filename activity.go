53f01ac8ad2b5c364108b41a89a69837cbb01153
carolina.soares <carolina.soares@litthub.com>
2020-05-04 14:28:41 +0100

    fix activity.go interface

--- activity.go
+++ activity.go
@@ -40,7 +40,7 @@ func (a *sendmail) Eval(ctx activity.Context) (done bool, err error) {
 	sender := ctx.GetInput("sender").(string)
 	apppass := ctx.GetInput("apppassword").(string)
 	ercpnt := ctx.GetInput("rcpnt").(string)
-	appointment := ctx.GetInput("appointment")
+	appointment := ctx.GetInput("appointment").(*string)
 	clinic := "Hospital Cuf descobertas"
 
 	var appointment1 string = fmt.Sprintf("%v",appointment)
